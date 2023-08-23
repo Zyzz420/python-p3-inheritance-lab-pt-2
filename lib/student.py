1728cb0526a768e0286bcf6a8b0a6139be2df239
Zyzz king <abdulaziz.komara@student.moringaschool.com>
2023-08-23 14:35:35 -0400

    Assignment complete

--- lib/student.py
+++ lib/student.py
@@ -1,14 +1,44 @@
-class Student:
-       def hello(self):
-        print("Hey there! I'm so excited to learn stuff.")
+class Song:
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
 
-       def raise_hand(self):
-        print("Pick me!")
-class ChattyStudent(Student):
-     def hello(self):
-        super().hello()
-        print("How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died...")
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        Song.add_song_to_count()
+        self.add_to_genres()
+        self.add_to_artists()
+        self.add_to_genre_count()
+        self.add_to_artist_count()
 
-     def raise_hand(self):
-        for _ in range(10):
-            super().raise_hand()
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    @classmethod
+    def add_to_genres(cls):
+        if cls.genre not in cls.genres:
+            cls.genres.append(cls.genre)
+
+    @classmethod
+    def add_to_artists(cls):
+        if cls.artist not in cls.artists:
+            cls.artists.append(cls.artist)
+
+    @classmethod
+    def add_to_genre_count(cls):
+        if cls.genre not in cls.genre_count:
+            cls.genre_count[cls.genre] = 1
+        else:
+            cls.genre_count[cls.genre] += 1
+
+    @classmethod
+    def add_to_artist_count(cls):
+        if cls.artist not in cls.artist_count:
+            cls.artist_count[cls.artist] = 1
+        else:
+            cls.artist_count[cls.artist] += 1
